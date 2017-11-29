f55eaa4c0e8f7aa108ffed70e87f4f14a3093aff
lgd <liguodong@zplay.cn>
2017-11-29 18:44:45 +0800

    控制样式过于复杂，暂时放弃

--- renderer-process/progress-bar.js
+++ renderer-process/progress-bar.js
@@ -1,52 +1,86 @@
 const $ = require('jquery')
 
-var progressBarRect = null
 var video = null
 
 var marks = []
 var cnt = 0
 
-$('.progress-bar').on('mousedown', (event) => {
+var currentBlock = null
+var blockBound, blockX, blockY;
+var blockOriginWith;
+var onLeftCtrl, onRightCtrl, onMiddleCtrl, onBottomCtrl;
+var downX;
 
-    var offset = $('.progress-bar').offset()
-    progressBarRect = {
-        'left': offset.left,
-        'right': offset.left + $('.progress-bar').width()
-    }
-    video = $('#palyer')[0]
+var progressBar = null;
+var e;
 
-    updateVideo(event);
+var redraw = false;
 
-    if ($('#ctrl-0').length) {
-        updateController(0, event.pageX)
-    } else {
-        addController(0, event.pageX)
-    }
+$(() => {
+
+    progressBar = $('.progress-bar');
+
+    progressBar.on('mousedown', onDown);
+    $(document).on('mousemove', onMove);
+    $(document).on('mouseup', onUp);
+    
+    progressBar.on('touchdown', onTouchDown)
+    $(document).on('touchmove', onTouchMove);
+    $(document).on('touchen', onTouchEnd);
 })
 
-$('body').on('mouseup', (event) => {
-    progressBarRect = null
-    video = null
-    // console.table(marks)
-})
+function onTouchDown(e) {
+    onDown(e.touches[0]);
+    e.preventDefault();
+}
 
-$('body').on('mousemove', (event) => {
-    updateVideo(event)
-})
+function onTouchMove(e) {
+    onMove(e.touches[0])
+}
 
+function  onTouchEnd(e) {
+    if (e.touches.length == 0) onUp(e.changedTouches[0]);
+}
 
-function addMark(pX) {
-    for (var i = 0; i < marks.length; i++) {
-        var mark = marks[i]
-        var t = space2time(pX, progressBarRect, video.duration)
-        if (mark.span.start < t && mark.span.end) {
-            return;
+function onMove(ee) {
+    clac(ee);
+    e = ee;
+    redraw = true;
+}
+
+function onUp(e) {
+    currentBlock = null;
+}
+
+animate();
+
+function animate(){
+    requestAnimationFrame(animate);
+    if (!redraw) return;
+    redraw = false;
+
+    if (!currentBlock) return;
+    if (onRightCtrl) {
+        currentBlock.style.width = Math.max(blockX, vh(4)) + 'px';
+    } 
+    if (onLeftCtrl) {
+        var currentWidth = Math.max(downX - e.clientX + blockOriginWith, vh(4));
+        if (currentWidth > vh(4)) {
+            currentBlock.style.width = currentWidth + 'px';
+            currentBlock.style.left = e.clientX + 'px';
         }
     }
 }
 
+var a = 0;
+function onDown(e) {
+    if (e.target === progressBar[0]){
+        addController(a++, e.pageX);
+    }
+}
+
+
 function addController(id, pageX) {
-    marks.push(mark(id, pageX))
     $('.controller-container').append(`<div id='ctrl-${id}' class='ctrl'></div>`)
     var ctrl = $(`#ctrl-${id}`)
     ctrl.append(`<div class='middle'></div>`)
@@ -60,23 +94,27 @@ function addController(id, pageX) {
     ctrl.find('.right').append(`<div class='triangle'></div>`)
     ctrl.find('.right').append(`<div class='line'></div>`)
     ctrl.css("left", pageX - vh(2))
-    
+   
     ctrl.dblclick(()=>{
         ctrl.remove();
     })
 
-    ctrl.on('mousedown', (event) => {
-        // ctrl.css('width', ctrl.width() + vh(2))
-        // ctrl.css('left', event.pageX - vh(2))
-    })
-
-    ctrl.on('mousemove', (event) => {
-        ctrl.css('left', event.pageX)
+    ctrl.on('mousedown', (e) => {
+        currentBlock = e.target;
+        clac(e);
+        downX = e.clientX;
+        blockOriginWith = blockBound.width;
+        e.preventDefault();
     })
 }
 
-function updateController(id, pageX) {
-    $(`#ctrl-${id}`).css("left", pageX - vh(2))
+function clac(e){
+    if (!currentBlock) return;
+    blockBound = currentBlock.getBoundingClientRect();
+    blockX = e.clientX - blockBound.left;
+    blockY = e.clientY - blockBound.top;
+    onLeftCtrl = blockX < vh(2)
+    onRightCtrl = blockX > blockBound.width - vh(2)
 }
 
 function mark(id, pageX) {
@@ -99,11 +137,11 @@ function vh(numb) {
     return $(window).height() * (numb) / 100.0;
 }
 
-function updateVideo(event) {
+function updateVideo(e) {
     if (progressBarRect == null || video == null || video == undefined) {
         return;
     }
-    video.currentTime = space2time(event.pageX, progressBarRect, video.duration)
+    video.currentTime = space2time(e.pageX, progressBarRect, video.duration)
 }
 
 /**
