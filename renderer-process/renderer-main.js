6d7da7c5272c80e2796e8beda998a2f53e3096ff
lgd <liguodong@zplay.cn>
2017-12-02 11:39:02 +0800

    回家做

--- renderer-process/renderer-main.js
+++ renderer-process/renderer-main.js
@@ -3,6 +3,11 @@ const $ = require('jquery')
 var utils = require('./utils')
 var marks = require('./mark')
 
+const MARGIN = utils.vh(2);
+const RECT_MARGIN = utils.vh(1);
+const BLOCK_TIME = 1;
+const BLOCK_SITE = 2;
+
 var video = null
 
 var cnt = 0
@@ -12,11 +17,23 @@ var middleCtrl = null;
 
 var blockBound, blockX, blockY;
 var blockOriginWith;
+
+// 时间控制器
 var onLeftCtrl, onRightCtrl, onMiddleCtrl;
 var ctrlLeftDelta, ctrlRightDelta;
 
+// 位置控制器
+var onLeftRect, onRightRect, onTopRect, onBottomRect, onMoveRect;
+var onResizing;
+var rectLeftDelta, rectTopDelta;
+
+// 当前操作的是哪个控制器：BLOCK_TIME 为时间控制器，BLOCK_SITE 为位置控制器
+var whichBlock = 0;
+
 var progressBar = null;
 var progressBarRect = null;
+
+var mobileScreen = null;
 
 var downX;
 var event;
@@ -25,155 +42,209 @@ var ctrlID = 0;
 
 var redraw = false;
 
-var MARGIN = utils.vh(2);
-
 $(() => {
 
-    progressBar = $('.progress-bar');
-    progressBarRect = progressBar[0].getBoundingClientRect();
+	progressBar = $('.progress-bar');
+	progressBarRect = progressBar[0].getBoundingClientRect();
 
-    progressBar.on('mousedown', onDown);
-    $(document).on('mousemove', onMove);
-    $(document).on('mouseup', onUp);
+	mobileScreen = $('.rectangle-container');
 
-    progressBar.on('touchdown', onTouchDown);
-    $(document).on('touchmove', onTouchMove);
-    $(document).on('touchen', onTouchEnd);
+	progressBar.on('mousedown', onDown);
+	$(document).on('mousemove', onMove);
+	$(document).on('mouseup', onUp);
+
+	progressBar.on('touchdown', onTouchDown);
+	$(document).on('touchmove', onTouchMove);
+	$(document).on('touchen', onTouchEnd);
 
 
-    $('#export').click(() => {
-        // ipc.send('save-file', data.getData())
-        console.log(progressBarRect)
-        console.log(JSON.stringify(marks.getMarks()));
-    });
+	$('#export').click(() => {
+		// ipc.send('save-file', data.getData())
+		console.log(progressBarRect)
+		console.log(JSON.stringify(marks.getMarks()));
+	});
 
-    $(window).resize(() => {
-        marks.reset();
+	$(window).resize(() => {
+		marks.reset();
 
-        var children = $('.controller-container').children(`.ctrl`);
-        for (var i=0; i<children.length; i++) {
-            children[i].remove();
-        }
-    })
+		var children = $('.controller-container').children(`.ctrl`);
+		for (var i = 0; i < children.length; i++) {
+			children[i].remove();
+		}
+	})
 })
 
 ipc.on('file-saved', (event, path) => {
-    console.log(path)
+	console.log(path)
 })
 
 function onTouchDown(e) {
-    onDown(e.touches[0]);
-    e.preventDefault();
+	onDown(e.touches[0]);
+	e.preventDefault();
 }
 
 function onTouchMove(e) {
-    onMove(e.touches[0])
+	onMove(e.touches[0])
 }
 
 function onTouchEnd(e) {
-    if (e.touches.length == 0) onUp(e.changedTouches[0]);
+	if (e.touches.length == 0) onUp(e.changedTouches[0]);
 }
 
 function onMove(ee) {
-    clac(ee);
-    event = ee;
-    redraw = true;
+	calc(ee);
+	event = ee;
+	redraw = true;
 }
 
 function onUp(e) {
-    currentBlock = null;
+	currentBlock = null;
+	whichBlock = 0;
 }
 
 animate();
 
 function animate() {
-    requestAnimationFrame(animate);
-    if (!redraw) return;
-    redraw = false;
+	requestAnimationFrame(animate);
+	if (!redraw) return;
+	redraw = false;
 
-    if (!currentBlock) return;
+	if (!currentBlock) return;
 
-    if (onLeftCtrl) {
-        var currentWidth = Math.max(downX - event.clientX + blockOriginWith, utils.vh(4));
-        if (currentWidth >= utils.vh(4)) {
-            middleCtrl.style.left = '0px';
-            currentBlock.style.width = currentWidth + 'px';
-            currentBlock.style.left = event.clientX + ctrlLeftDelta + 'px';
-            var spanStart = event.clientX + ctrlLeftDelta + utils.vh(2)
-            marks.getActivedMark().span.start = spanStart;
-            marks.getActivedMark().span.loopStart = spanStart
-            updateVideo(spanStart);
-        }
-    }
+	if (whichBlock == BLOCK_TIME) {
+		if (onLeftCtrl) {
+			var currentWidth = Math.max(downX - event.clientX + blockOriginWith, utils.vh(4));
+			if (currentWidth >= utils.vh(4)) {
+				middleCtrl.style.left = '0px';
+				currentBlock.style.width = currentWidth + 'px';
+				currentBlock.style.left = event.clientX + ctrlLeftDelta + 'px';
+				var spanStart = event.clientX + ctrlLeftDelta + utils.vh(2)
+				marks.getActivedMark().span.start = spanStart;
+				marks.getActivedMark().span.loopStart = spanStart
+				updateVideo(spanStart);
+			}
+		}
 
-    if (onRightCtrl) {
-        currentBlock.style.width = Math.max(blockX + ctrlRightDelta, utils.vh(4)) + 'px';
-        var spanEnd = event.clientX - utils.vh(2) + ctrlRightDelta;;
-        marks.getActivedMark().span.end = spanEnd;
-        updateVideo(spanEnd);
-    }
-   
+		if (onRightCtrl) {
+			currentBlock.style.width = Math.max(blockX + ctrlRightDelta, utils.vh(4)) + 'px';
+			var spanEnd = event.clientX - utils.vh(2) + ctrlRightDelta;;
+			marks.getActivedMark().span.end = spanEnd;
+			updateVideo(spanEnd);
+		}
 
-    if (onMiddleCtrl) {
-        middleCtrl.style.left = blockX - utils.vh(2) + 'px';
-        marks.getActivedMark().span.loopStart = event.clientX;
-        updateVideo(event.clientX);
-    }
+
+		if (onMiddleCtrl) {
+			middleCtrl.style.left = blockX - utils.vh(2) + 'px';
+			marks.getActivedMark().span.loopStart = event.clientX;
+			updateVideo(event.clientX);
+		}
+	} else if (whichBlock == BLOCK_SITE) {
+		if (onLeftRect) {
+			console.log('left')
+		}
+		if (onRightRect) {
+			console.log('right')
+			currentBlock.style.width = Math.max(blockX, utils.vh(10)) + 'px';
+		}
+		if (onMove){
+			console.log('move')
+			currentBlock.style.top = blockY + 'px';
+			currentBlock.style.left = blockX + 'px';
+		}
+	}
 }
 
 function onDown(e) {
-    if (e.target === progressBar[0]) {
-        addController(ctrlID++, e.pageX);
-        video = $(".palyer")[0];
-    }
+	if (e.target === progressBar[0]) {
+		addController(ctrlID++, e.pageX);
+		video = $(".palyer")[0];
+	}
 }
 
 function addController(id, pageX) {
-    $('.controller-container').append(`<div id='ctrl-${id}' class='ctrl'></div>`)
-    var ctrl = $(`#ctrl-${id}`)
-    ctrl.append(`<div class='middle'></div>`)
-    ctrl.append(`<div class='left'></div>`)
-    ctrl.find('.left').append(`<div class='triangle'></div>`)
-    ctrl.find('.left').append(`<div class='line'></div>`)
-    ctrl.append(`<div class='right'></div>`)
-    ctrl.find('.right').append(`<div class='triangle'></div>`)
-    ctrl.find('.right').append(`<div class='line'></div>`)
-    ctrl.css("left", pageX - utils.vh(2))
+	$('.controller-container').append(`<div id='ctrl-${id}' class='ctrl'></div>`)
+	var ctrl = $(`#ctrl-${id}`)
+	ctrl.append(`<div class='middle'></div>`)
+	ctrl.append(`<div class='left'></div>`)
+	ctrl.find('.left').append(`<div class='triangle'></div>`)
+	ctrl.find('.left').append(`<div class='line'></div>`)
+	ctrl.append(`<div class='right'></div>`)
+	ctrl.find('.right').append(`<div class='triangle'></div>`)
+	ctrl.find('.right').append(`<div class='line'></div>`)
+	ctrl.css("left", pageX - utils.vh(2))
 
-    ctrl.dblclick(() => {
-        ctrl.remove();
-        marks.remove(id);
-    })
+	var rect = addRect(id);
 
-    ctrl.on('mousedown', (e) => {
-        currentBlock = e.target;
-        marks.setActivemark(id);
-        middleCtrl = ctrl.find('.middle')[0];
-        clac(e);
-        downX = e.clientX;
-        ctrlLeftDelta = blockBound.left - e.clientX;
-        ctrlRightDelta = blockBound.right - e.clientX;
-        blockOriginWith = blockBound.width;
-        e.preventDefault();
-    })
+	ctrl.dblclick(() => {
+		ctrl.remove();
+		rect.remove();
+		marks.remove(id);
+	})
 
-    marks.add(id);
+	ctrl.on('mousedown', (e) => {
+		whichBlock = BLOCK_TIME;
+		currentBlock = e.target;
+		marks.setActivemark(id);
+		middleCtrl = ctrl.find('.middle')[0];
+		downX = e.clientX;
+
+		calc(e);
+		redraw = true;
+		ctrlLeftDelta = blockBound.left - e.clientX;
+		ctrlRightDelta = blockBound.right - e.clientX;
+		blockOriginWith = blockBound.width;
+
+		e.preventDefault();
+	})
+
+	rect.on(`mousedown`, (e) => {
+		whichBlock = BLOCK_SITE;
+		currentBlock = e.target;
+		downX = e.clientX;
+
+		calc(e);
+		redraw = true;
+		blockOriginWith = blockBound.width;
+		onResizing = onLeftRect && onRightRect;
+		e.preventDefault();
+	})
+
+	marks.add(id);
 }
 
-function clac(e) {
-    if (!currentBlock) return;
-    blockBound = currentBlock.getBoundingClientRect();
-    blockX = e.clientX - blockBound.left;
-    blockY = e.clientY - blockBound.top;
-    onLeftCtrl = blockX < MARGIN
-    onRightCtrl = blockX > blockBound.width - MARGIN
-    onMiddleCtrl = MARGIN < blockX && blockX < blockBound.width - MARGIN;
+function addRect(id) {
+	mobileScreen.append(`<div id='rect-${id}' class='rectangle'></div>`);
+	var rect = $(`#rect-${id}`);
+	rect.append(`<input type='checkbox' class='checkbox-left'></input>`);
+	rect.append(`<input type='checkbox' class='checkbox-top'></input>`);
+	rect.append(`<input type='checkbox' class='checkbox-right'></input>`);
+	rect.append(`<input type='checkbox' class='checkbox-bottom'></input>`);
+	rect.append(`<input type='checkbox' class='checkbox-center'></input>`);
+	return rect;
+}
+
+function calc(e) {
+	if (!currentBlock) return;
+
+	blockBound = currentBlock.getBoundingClientRect();
+	blockX = e.clientX - blockBound.left;
+	blockY = e.clientY - blockBound.top;
+
+	if (whichBlock == BLOCK_TIME) {
+		onLeftCtrl = blockX < MARGIN;
+		onRightCtrl = blockX > blockBound.width - MARGIN;
+		onMiddleCtrl = MARGIN < blockX && blockX < blockBound.width - MARGIN;
+	} else if (whichBlock == BLOCK_SITE) {
+		onLeftRect = blockX < RECT_MARGIN;
+		onRightRect = blockX > blockBound.width - RECT_MARGIN;
+		onMoveRect = !onResizing && (blockX > RECT_MARGIN && blockX < blockBound.width - RECT_MARGIN) && (blockY > RECT_MARGIN && blockY < blockBound.height - RECT_MARGIN);
+	}
 }
 
 function updateVideo(progressBarX) {
-    if (currentBlock && progressBarRect && video) {
-        video.currentTime = space2time(progressBarX, progressBarRect, video.duration);
-    }
+	if (currentBlock && progressBarRect && video) {
+		video.currentTime = space2time(progressBarX, progressBarRect, video.duration);
+	}
 }
 
 /**
@@ -183,13 +254,13 @@ function updateVideo(progressBarX) {
  * @param {时间总长度} duration  时间总长度
  */
 function space2time(pageX, clientRect, duration) {
-    var start = clientRect.left
-    var end = clientRect.right
-    if (pageX < start) {
-        return 0;
-    } else if (pageX > end) {
-        return duration;
-    } else {
-        return (pageX - start) / (end - start) * duration;
-    }
+	var start = clientRect.left
+	var end = clientRect.right
+	if (pageX < start) {
+		return 0;
+	} else if (pageX > end) {
+		return duration;
+	} else {
+		return (pageX - start) / (end - start) * duration;
+	}
 }
